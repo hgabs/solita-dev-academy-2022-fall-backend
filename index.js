339dff00b6e3558386ab7c8c30c90a49695e7056
hgabs <14847474+hgabs@users.noreply.github.com>
2022-08-08 22:30:01 +0300

    Add apiRouter to the backend

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const { apiRouter } = require('./routes');
 const { PORT } = require('./config');
 
 
 app.use(morgan('tiny'));
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}\n`);
