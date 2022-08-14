62fc992cbaa92535ec8ca110024e59c2466116dc
hgabs <14847474+hgabs@users.noreply.github.com>
2022-08-14 19:07:37 +0300

    Add Digitransit HSL API URL variable to config

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
 const PORT = process.env.PORT || 9000;
+const DIGITRANSIT_HSL_API = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
 
 
 module.exports = {
-    PORT
+    PORT,
+    DIGITRANSIT_HSL_API
 };
