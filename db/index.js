602d0aef3e02919fbf645a4d035cef1ce25be810
hgabs <14847474+hgabs@users.noreply.github.com>
2022-08-15 06:21:52 +0300

    Add password variables to database options

--- db/index.js
+++ db/index.js
@@ -7,6 +7,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '..', '.env')});
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
 });
