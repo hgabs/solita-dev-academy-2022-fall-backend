d11eb883de511be1e8a5a9c7bf04ef90a6fc04ac
hgabs <14847474+hgabs@users.noreply.github.com>
2022-08-15 06:23:26 +0300

    Add drop statement, missing constraint and indexes

--- db/scripts/create.sql
+++ db/scripts/create.sql
@@ -1,9 +1,12 @@
+DROP TABLE IF EXISTS stations CASCADE;
+
 CREATE TABLE IF NOT EXISTS stations (
     id INT NOT NULL,
     name VARCHAR(255) NOT NULL
 );
 
 ALTER TABLE stations ADD CONSTRAINT pk_stations PRIMARY KEY (id);
+CREATE INDEX ON stations (name);
 
 CREATE TABLE IF NOT EXISTS journeys (
     id SERIAL,
@@ -15,5 +18,12 @@ CREATE TABLE IF NOT EXISTS journeys (
     duration INT NOT NULL
 );
 
+ALTER TABLE journeys ADD CONSTRAINT pk_journeys PRIMARY KEY (id);
+
 ALTER TABLE journeys ADD CONSTRAINT fk_journeys_stations_depature_station_id FOREIGN KEY (departure_station_id) REFERENCES stations (id);
 ALTER TABLE journeys ADD CONSTRAINT fk_journeys_stations_arrival_station_id FOREIGN KEY (arrival_station_id) REFERENCES stations (id);
+
+CREATE INDEX ON journeys (departure);
+CREATE INDEX ON journeys (arrival);
+CREATE INDEX ON journeys (distance);
+CREATE INDEX ON journeys (duration);
